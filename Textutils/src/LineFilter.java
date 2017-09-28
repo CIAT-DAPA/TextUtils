7665e4ec81124a8045602f45c39bd44f9ffd6286
Daniel Amariles <daniel.amariles88@gmail.com>
2017-09-28 14:46:46 -0500

    filtering

--- Textutils/src/LineFilter.java
+++ Textutils/src/LineFilter.java
@@ -3,45 +3,56 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LineFilter {
 
+	private Map<String, Integer> colIndex;
+	private static final String SEPARATOR = "\t";
+
 	public static void main(String[] args) {
-		LineFilter app=new LineFilter();
+		LineFilter app = new LineFilter();
 		app.run();
 	}
-	
-	private void run(){
+
+	private void run() {
 
 		String jump = "\r\n";
 		// if (args.length > 0) {
 		File input = new File("file.csv");
-		File filterSet = new File("filter.txt");
 		File output = new File("file_filtered.csv");
-		Set<String> filters = loadFilters(filterSet);
 
-		try (BufferedWriter writer = new BufferedWriter(new PrintWriter(output));
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input)))) {
+		try (BufferedWriter writer = new BufferedWriter(
+				new PrintWriter(new OutputStreamWriter(new FileOutputStream(output), "UTF-8")));
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(new FileInputStream(input), "UTF-8"))) {
 
+			/* header */
 			String line = reader.readLine();
+			colIndex = getColumnsIndex(line);
+			writer.write(line);
+			writer.write(jump);
+			/**/
+
+			line = reader.readLine();
 			while (line != null) {
-				for (String filter : filters) {
-					if (line.contains(filter)) {
-						writer.write(line);
-						writer.write(jump);
-					}
+				if (isTarget(line)) {
+					writer.write(line);
+					writer.write(jump);
 				}
 				line = reader.readLine();
 			}
 
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found " + input.getAbsolutePath());
-			e.printStackTrace();
 		} catch (IOException e) {
 			System.out.println("Cannot read " + input.getAbsolutePath());
 		}
@@ -50,26 +61,80 @@ public class LineFilter {
 		// System.out.println("File not provided in arguments");
 		// }
 
-	
 	}
-	
-	private Set<String> loadFilters(File filterSet){
+
+	private boolean isTarget(String line) {
+		boolean flag = false;
+		line+="\t ";
+		String[] values = line.split(SEPARATOR);
+
+		if (!line.contains("SPECIES")) {
+			return false;
+		}
+		
+		/* excluding issues */
+		flag = false;
+		Set<String> issues = new LinkedHashSet<>();
+		issues.add("COORDINATE_OUT_OF_RANGE");
+		issues.add("COUNTRY_COORDINATE_MISMATCH");
+		issues.add("ZERO_COORDINATE");
+
+		matchIssue: for (String issue : issues) {
+			if (values[colIndex.get("issue")].contains(issue)) {
+				flag = true;
+				break matchIssue;
+			}
+		}
+		if (!flag) {
+			return flag; // return false 
+		}
+		/**/
+
+		/* matching with taxa */
+		flag = false;
+		File taxaFile = new File("taxa.txt");
+		Set<String> taxa = loadFilters(taxaFile);
+		matchTaxa: for (String taxon : taxa) {
+			if (values[colIndex.get("taxonkey")].equals(taxon)) {
+				flag = true;
+				break matchTaxa;
+			}
+		}
+		if (!flag) {
+			return flag; // return false in case taxon is not found
+		}
+		/**/
+
+		return flag;
+	}
+
+	private Set<String> loadFilters(File vocabularyFile) {
 		Set<String> filters = new LinkedHashSet<String>();
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filterSet)))) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(vocabularyFile)))) {
 
 			String line = reader.readLine();
 			while (line != null) {
-				filters.add(line);
+				if (!line.isEmpty()) {
+					filters.add(line);
+				}
 				line = reader.readLine();
 			}
 
 		} catch (FileNotFoundException e) {
-			System.out.println("File not found " + filterSet.getAbsolutePath());
-			e.printStackTrace();
+			System.out.println("File not found " + vocabularyFile.getAbsolutePath());
 		} catch (IOException e) {
-			System.out.println("Cannot read " + filterSet.getAbsolutePath());
+			System.out.println("Cannot read " + vocabularyFile.getAbsolutePath());
 		}
 		return filters;
+	}
+
+	private Map<String, Integer> getColumnsIndex(String line) {
+		Map<String, Integer> colIndex = new LinkedHashMap<String, Integer>();
+		String[] columnNames = line.split(SEPARATOR);
+		for (int i = 0; i < columnNames.length; i++) {
+			colIndex.put(columnNames[i], i);
+		}
+		return colIndex;
 	}
 
 }
