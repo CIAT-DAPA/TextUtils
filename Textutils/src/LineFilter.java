a53c7e841d80c1017d006110b0ab8d948362a80a
Daniel Amariles <daniel.amariles88@gmail.com>
2017-09-28 10:09:22 -0500

    filtering

--- Textutils/src/LineFilter.java
+++ Textutils/src/LineFilter.java
@@ -6,38 +6,70 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class LineFilter {
 
 	public static void main(String[] args) {
-		String jump = "\r\n";
-		//if (args.length > 0) {
-			File input = new File("file.csv");
-			File output = new File("file_filtered.csv");
-			String line = "";
-			try (BufferedWriter writer = new BufferedWriter(new PrintWriter(output));
-					BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input)))) {
+		LineFilter app=new LineFilter();
+		app.run();
+	}
+	
+	private void run(){
 
-				line = reader.readLine();
-				while (line != null) {
-					if (line.contains("a")) {
+		String jump = "\r\n";
+		// if (args.length > 0) {
+		File input = new File("file.csv");
+		File filterSet = new File("filter.txt");
+		File output = new File("file_filtered.csv");
+		Set<String> filters = loadFilters(filterSet);
+
+		try (BufferedWriter writer = new BufferedWriter(new PrintWriter(output));
+				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input)))) {
+
+			String line = reader.readLine();
+			while (line != null) {
+				for (String filter : filters) {
+					if (line.contains(filter)) {
 						writer.write(line);
 						writer.write(jump);
 					}
-					line = reader.readLine();
 				}
-
-			} catch (FileNotFoundException e) {
-				System.out.println("File not found " + input.getAbsolutePath());
-				e.printStackTrace();
-			} catch (IOException e) {
-				System.out.println("Cannot read " + input.getAbsolutePath());
+				line = reader.readLine();
 			}
 
-		//} else {
-		//	System.out.println("File not provided in arguments");
-		//}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found " + input.getAbsolutePath());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Cannot read " + input.getAbsolutePath());
+		}
 
+		// } else {
+		// System.out.println("File not provided in arguments");
+		// }
+
+	
+	}
+	
+	private Set<String> loadFilters(File filterSet){
+		Set<String> filters = new LinkedHashSet<String>();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filterSet)))) {
+
+			String line = reader.readLine();
+			while (line != null) {
+				filters.add(line);
+				line = reader.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found " + filterSet.getAbsolutePath());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Cannot read " + filterSet.getAbsolutePath());
+		}
+		return filters;
 	}
 
 }
