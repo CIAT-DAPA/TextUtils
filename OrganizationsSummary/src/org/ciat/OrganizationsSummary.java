f18f3602f592969d6c3b047308bed687bb093678
Daniel Amariles <daniel.amariles88@gmail.com>
2018-01-05 15:18:56 -0500

    adding Organizations Summary project

---
+++ OrganizationsSummary/src/org/ciat/OrganizationsSummary.java
@@ -0,0 +1,154 @@
+package org.ciat;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class OrganizationsSummary {
+
+	private Map<String, Integer> colIndex;
+	private static final String SEPARATOR = "\t";
+
+	public static void main(String[] args) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date));
+
+		String fileName = "gbif.csv";
+		if (args.length > 0) {
+			fileName = args[0];
+		} else {
+			System.out.println("File not provided in arguments, using " + fileName + " as default");
+		}
+
+		OrganizationsSummary app = new OrganizationsSummary();
+		app.extract(fileName);
+
+		date = new Date();
+		System.out.println();
+		System.out.println(dateFormat.format(date));
+	}
+
+	private void extract(String fileName) {
+
+		File input = new File(fileName);
+		File output = new File("orgs.csv");
+
+		try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(output)));
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(new FileInputStream(input), "UTF-8"))) {
+
+			/* header */
+			String line = reader.readLine();
+			colIndex = getColumnsIndex(line);
+			/* */
+
+			/* progress bar */
+			ProgressBar bar = new ProgressBar();
+			int exp = (int) Math.ceil((input.length() + "").length()) + 1;
+			int dimensionality = (int) Math.pow(2, exp);
+			int total = Math.toIntExact(input.length() / dimensionality);
+			long done = line.length();
+			int lineNumber = 0;
+			System.out.println("Reading " + input.length() / 1024 + "KB");
+			System.out.println("Updating progress each " + dimensionality + "KB read");
+			/* */
+
+			Set<String> publishers = new LinkedHashSet<String>();
+			line = reader.readLine();
+			while (line != null) {
+				line += SEPARATOR + " ";
+				String[] values = line.split(SEPARATOR);
+				String code = values[colIndex.get("institutioncode")];
+				String pub = values[colIndex.get("publishingorgkey")];
+				if (!publishers.contains(pub)) {
+					writer.println(code + SEPARATOR + pub + SEPARATOR + fetchInfo(pub));
+					publishers.add(pub);
+				}
+
+				/* show progress */
+				done += line.length();
+				if (++lineNumber % dimensionality == 0) {
+					bar.update(Math.toIntExact(done / dimensionality), total);
+				}
+				/* */
+
+				line = reader.readLine();
+
+			}
+			bar.update(Math.toIntExact(done / dimensionality), total);
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found " + input.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Map<String, Integer> getColumnsIndex(String line) {
+		Map<String, Integer> colIndex = new LinkedHashMap<String, Integer>();
+		String[] columnNames = line.split(SEPARATOR);
+		for (int i = 0; i < columnNames.length; i++) {
+			colIndex.put(columnNames[i], i);
+		}
+		return colIndex;
+	}
+
+	private CharSequence fetchInfo(String key) {
+
+		// make connection
+
+		URLConnection urlc;
+		try {
+			URL url = new URL("http://api.gbif.org/v1/organization/" + key + "");
+
+			urlc = url.openConnection();
+			// use post mode
+			urlc.setDoOutput(true);
+			urlc.setAllowUserInteraction(false);
+
+			// send query
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()))) {
+
+				// get result
+				String result = br.readLine();
+				String[] values = result.split("\",\"");
+				result = "";
+				for (int i = 0; i<values.length && i < 11; i++) {
+					String[] pair = values[i].split("\":\"");
+					if (pair.length == 2) {
+						result += pair[1] + SEPARATOR;
+					}
+				}
+				return result;
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
